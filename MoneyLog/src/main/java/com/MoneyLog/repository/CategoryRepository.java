d5e5ab06549ed92c9636ff3a4fcaadfed0c33938
camellia-ver <jakahi435@gmail.com>
2026-07-14 13:32:59 +0900

    feat: add CategoryRepository class

---
+++ MoneyLog/src/main/java/com/MoneyLog/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.MoneyLog.repository;
+
+import com.MoneyLog.Model.Category;
+import com.MoneyLog.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    boolean existsByUserAndName(User user, String name);
+}
