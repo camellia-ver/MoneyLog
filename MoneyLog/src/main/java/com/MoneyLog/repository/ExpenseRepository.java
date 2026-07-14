0f61d8417a888f03ca2c4b9528cf0237d9a6ebea
camellia-ver <jakahi435@gmail.com>
2026-07-14 13:37:35 +0900

    feat: add ExpenseRepository interface

---
+++ MoneyLog/src/main/java/com/MoneyLog/repository/ExpenseRepository.java
@@ -0,0 +1,13 @@
+package com.MoneyLog.repository;
+
+import com.MoneyLog.Model.Expense;
+import com.MoneyLog.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findByUser(User user);
+}
