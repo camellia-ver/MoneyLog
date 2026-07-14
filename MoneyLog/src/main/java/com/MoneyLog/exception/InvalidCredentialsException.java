504ab2b6eff2376df1b575d3547173873572aad5
camellia-ver <jakahi435@gmail.com>
2026-07-14 14:28:03 +0900

    feat: add InvalidCredentialsException class

---
+++ MoneyLog/src/main/java/com/MoneyLog/exception/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.MoneyLog.exception;
+
+public class InvalidCredentialsException extends Exception{
+    public InvalidCredentialsException(String message){
+        super(message);
+    }
+}
