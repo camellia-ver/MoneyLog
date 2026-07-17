7fee0fa4c2605dd9b125aa9d938e205ec88e9321
camellia-ver <jakahi435@gmail.com>
2026-07-17 15:01:53 +0900

    feat: add logout api

--- MoneyLog/src/main/java/com/MoneyLog/api/AuthApiController.java
+++ MoneyLog/src/main/java/com/MoneyLog/api/AuthApiController.java
@@ -7,6 +7,7 @@ import com.MoneyLog.security.JwtTokenProvider;
 import com.MoneyLog.service.AuthService;
 import com.MoneyLog.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +22,11 @@ public class AuthApiController {
     @PostMapping("/login")
     public LoginResponseDto login(@RequestBody LoginRequestDto request){
         return authService.login(request);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(){
+        // TODO: 지금 당장은 특별한 로직 없음 차후 기능 확장 가능
+        return ResponseEntity.ok().build();
     }
 }
