ac7551edd163984e11e5988e38b2eb20d03d7bcf
camellia-ver <jakahi435@gmail.com>
2026-07-31 13:25:17 +0900

    feat: update api.js

--- MoneyLog/src/main/resources/static/js/api.js
+++ MoneyLog/src/main/resources/static/js/api.js
@@ -8,4 +8,17 @@ async function apiRequest(path, options = {}) {
         ...(token ? {Authorization: `Bearer ${token}`} : {}),
         ...options.headers,
     };
+
+    const response = await fetch(BASE_URL + path,{
+        ...options,
+        headers,
+    });
+
+    if (!response.ok){
+        const errorBody = await response.json();
+        throw new Error(errorBody.message);
+    }
+
+    const text = await response.text()
+    return text ? JSON.parse(text) : null;
 }
