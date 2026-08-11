12680ac3c6fc6699fd8859d3f575932db279a56a
camellia-ver <jakahi435@gmail.com>
2026-08-11 16:04:54 +0900

    fix: fix API 호출이 불가능한 문제 해결

--- MoneyLog/src/main/resources/static/js/api.js
+++ MoneyLog/src/main/resources/static/js/api.js
@@ -1,4 +1,7 @@
-const BASE_URL = "http://localhost:8080"
+const BASE_URL =
+    window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+        ? "http://localhost:8080"
+        : "";
 
 async function apiRequest(path, options = {}) {
   const token = localStorage.getItem("token");
